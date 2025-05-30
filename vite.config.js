297851b99900f030497fb2be4eb2d624c63fb733
Sujot Singh <ssing231@uic.edu>
2025-05-30 18:41:29 -0500

    Trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/3xcess.github.io',
   plugins: [react()],
-  base: '/3xcess.github.io/'
 });
