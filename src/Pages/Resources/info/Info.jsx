da0ec363ff9ef2ffb07de33cf778480697d9cd5b
Sujot Singh <ssing231@uic.edu>
2025-05-30 14:18:41 -0500

    Added contact info
    Logos

--- src/Pages/Resources/info/Info.jsx
+++ src/Pages/Resources/info/Info.jsx
@@ -1,9 +1,18 @@
+import github from "../../../assets/Github.svg"
+import linkedin from "../../../assets/Linkedin.svg"
+import gmail from "../../../assets/Gmail.svg"
+
 const Info = () => {
   return (
     <>
-      <div className="jumbotron" style={{ backgroundColor: '#F7F7F2' }}>
+      <div className="jumbotron" style={{ backgroundColor: '#F7F7F2', position: 'relative' }}>
         <div className="container">
           <div className="row flex-column-reverse">
+            <div className="col-md-12 d-flex justify-content-end align-items-bottom">
+              <a className='px-2 text-right' href="mailto:sujots92@gmail.com"><img width="20" height="20" src={gmail} className="logo" alt="mail" style={{verticalAlign: 'middle', marginTop: "5px"}}/></a>
+              <a className='px-2 text-right' href="https://www.linkedin.com/in/sujot/"><img width="20" height="20" src={linkedin} className="logo" alt="linkedin" /></a>
+              <a className='px-2 text-right' href="https://github.com/3xcess"><img width="20" height="20" src={github} className="logo" alt="github" /></a>
+            </div>
             <h1 className="display-3 col-md-1 text-center" style={{color: "#39392D"}}>About</h1>
           </div>
         </div>
--- src/Pages/Resources/landing/landing.jsx
+++ src/Pages/Resources/landing/landing.jsx
@@ -1,5 +1,8 @@
 import './landing.css'
 import Sujot from '../../../assets/Images/Sujot.JPG'
+import github from "../../../assets/Github.svg"
+import linkedin from "../../../assets/Linkedin.svg"
+import gmail from "../../../assets/Gmail.svg"
 
 const Landing = () => {
   return (
@@ -11,8 +14,14 @@ const Landing = () => {
             <h1 className="display-3 d-flex justify-content-center px-2" style={{color: "#39392D"}}>Sujot</h1>
             <h1 className="display-3 d-flex justify-content-center px-1" style={{color: "#39392D"}}>Singh</h1>
           </div>
-
           <h4 className='text-center' style={{ color: '#39392D' }}>Short Desc</h4>
+
+          <div className="col-md-12 d-flex justify-content-end align-items-bottom">
+              <a className='px-2 text-right' href="mailto:sujots92@gmail.com"><img width="20" height="20" src={gmail} className="logo" alt="mail" style={{verticalAlign: 'middle', marginTop: "5px"}}/></a>
+              <a className='px-2 text-right' href="https://www.linkedin.com/in/sujot/"><img width="20" height="20" src={linkedin} className="logo" alt="linkedin" /></a>
+              <a className='px-2 text-right' href="https://github.com/3xcess"><img width="20" height="20" src={github} className="logo" alt="github" /></a>
+          </div>
+          
         </div>
       </div>
 
