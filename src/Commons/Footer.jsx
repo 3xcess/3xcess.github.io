6957952f2d64efc31887a543ca24ba5b8bf36cb4
Sujot Singh <ssing231@uic.edu>
2025-05-30 20:28:21 -0500

    Footer minor Changes

--- src/Commons/Footer.jsx
+++ src/Commons/Footer.jsx
@@ -3,8 +3,8 @@ const Footer = () => {
     <>
       <footer className="container text-center">
         <hr />
-        <p style={{margin:"1px"}}>Made with ❤️ by Sujot Singh</p>
-        <a href="https://github.com/3xcess/3xcess.github.io" style={{margin:"-2px", textDecoration:"underline", color:"#000000"}}>Feel Free to make it your own 😁</a>
+        <p style={{margin:"1px"}}>Made with 💚 by Sujot Singh</p>
+        <a href="https://github.com/3xcess/3xcess.github.io" style={{margin:"-2px", textDecoration:"underline", color:"#000000"}}>Feel Free to make it your own :)</a>
       </footer>
       
     </>
